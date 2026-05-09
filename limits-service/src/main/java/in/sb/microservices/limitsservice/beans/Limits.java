7d7ed240363f5d6aa2e326e6bdeb2feb05e2e821
Naman Rawat <namanrawat90.nr@gmail.com>
2026-05-09 23:43:16 +0530

    01_limits-service

---
+++ limits-service/src/main/java/in/sb/microservices/limitsservice/beans/Limits.java
@@ -0,0 +1,31 @@
+package in.sb.microservices.limitsservice.beans;
+
+public class Limits {
+
+    private int minimun;
+    private int maximum;
+
+    public Limits(){
+        super();
+    }
+    public Limits(int minimun, int maximum) {
+        this.minimun = minimun;
+        this.maximum = maximum;
+    }
+
+    public int getMinimun() {
+        return minimun;
+    }
+
+    public void setMinimun(int minimun) {
+        this.minimun = minimun;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+}
